eb12088af8e4d6d990ea4121c4675002ca8baa93
sergey-repin <56038916+sergey-repin@users.noreply.github.com>
2019-12-11 19:16:47 +0300

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,16 @@
+a = int (input("a = "))
+a = 7
+>>> b = int (input("b = "))
+b = 5
+op = input("add/sub/mul/div:")
+add/sub/mul/div:sub
+if op == "add":
+    c = a + b
+elif op == "sub":
+    c = a - b
+elif op == "mul":
+    c = a * b
+elif op == "div":
+    c = a / b
+else:
+    ñ = "Error"
